6512eb8b63f2167864e69584914d342b0a309674
Mayank Kumar Singh <mayanksim0022@gmail.com>
2026-03-27 22:42:30 +0530

    feat: add URL shortening and redirect APIs

---
+++ src/main/java/com/mayank/urlshortener/controller/UrlController.java
@@ -0,0 +1,31 @@
+package com.mayank.urlshortener.controller;
+
+import com.mayank.urlshortener.service.UrlService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api")
+public class UrlController {
+
+    @Autowired
+    private UrlService service;
+
+    @PostMapping("/shorten")
+    public String shorten(@RequestParam String url) {
+        return service.shortenUrl(url);
+    }
+
+    @GetMapping("/{shortUrl}")
+    public ResponseEntity<Void> redirect(@PathVariable String shortUrl) {
+        String originalUrl = service.getOriginalUrl(shortUrl);
+
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .location(URI.create(originalUrl))
+                .build();
+    }
+}
