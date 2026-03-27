0e2256cadf5a91d0d50f9544db188cb04d4cc47b
Mayank Kumar Singh <mayanksim0022@gmail.com>
2026-03-27 22:22:12 +0530

    Initial commit - URL shortener project

---
+++ src/test/java/com/mayank/urlshortener/UrlshortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayank.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
