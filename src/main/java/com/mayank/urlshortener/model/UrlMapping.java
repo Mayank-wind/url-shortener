b8c31bef63896ae2a710049b19b264e306ba89da
Mayank Kumar Singh <mayanksim0022@gmail.com>
2026-03-27 22:34:52 +0530

    feat: add UrlRepository for database operations

---
+++ src/main/java/com/mayank/urlshortener/model/UrlMapping.java
@@ -0,0 +1,17 @@
+package com.mayank.urlshortener.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class UrlMapping {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String shortUrl;
+    private String originalUrl;
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/com/mayank/urlshortener/repository/UrlRepository.java
@@ -0,0 +1,10 @@
+package com.mayank.urlshortener.repository;
+
+import com.mayank.urlshortener.model.UrlMapping;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UrlRepository extends JpaRepository<UrlMapping, Long> {
+    Optional<UrlMapping> findByShortUrl(String shortUrl);
+}
